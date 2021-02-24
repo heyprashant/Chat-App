3d18701acd8cd8b05e051a672266a6ec5d34511b
heyprashant <prashant.p7804@gmail.com>
2021-02-24 19:01:26 +0530

    add socket.io & location share

--- src/index.js
+++ src/index.js
@@ -1,20 +1,46 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
-
-// Define pahts for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 
-// Setup static directory to serve, express.static is a middleware
 app.use(express.static(publicDirectoryPath))
 
-// app.get('', (req, res) => {
-//     res.render()
-// })
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+   
+    socket.emit('message', 'Welcome!' )
+    socket.broadcast.emit('message', 'A new user has joined!')
 
-app.listen(port, () => {
-    console.log('Server is up and running!')
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if(filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.emit('message', message)
+        callback()
+    })
+
+    socket.on('disconnect', () =>{
+        io.emit('message', 'A user has left!')
+    })
+
+    socket.on('sendLocation', ( {latitude, longitude}, callback ) => {
+        io.emit('message', `https://google.com/maps?q=${latitude},${longitude}`)
+        callback()
+    })
+ 
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 })
